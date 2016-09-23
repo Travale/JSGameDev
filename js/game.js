ec642d33873c3542027acb4510d115652911e365
Travis Wilkins <uchiwasteel@gmail.com>
2016-09-23 11:49:21 -0600

    game

---
+++ js/game.js
@@ -0,0 +1,29 @@
+var mainPlayer = {
+    name: "Player",
+    rank: "Pawn",
+    speed: 10
+    power: 1,
+
+    messanger = function (message) {
+        alert(message);
+
+    }
+}
+
+mainPlayer.name = prompt("Enter your name");
+
+alert("Welcome"+ mainPlayer.name);
+
+var phrase0 = [prompt("Please enter mother's maiden name."),
+            prompt("Enter your phone number"),
+            prompt("enter your credit card number.")];
+
+for (var i = 0 ; i < phrases.length ; i++){
+    mainPlayer.messenger(phrases[i]);
+}
+
+
+
+
+
+
