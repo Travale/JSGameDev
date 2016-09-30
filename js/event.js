49aaeb3479e7c0a5bc9ff8ef09c9f404254a5441
Travis Wilkins <uchiwasteel@gmail.com>
2016-09-30 11:49:29 -0600

    buttons

--- js/event.js
+++ js/event.js
@@ -1,16 +1,36 @@
-var button = document.getElementById('button1')
+//UserName Button
+var userName;
+console.log(userName);
 
-var myFunction = function myFunctionHandler () {
-    alert("Hello World");
+var button = document.getElementById('name')
+var enterName = function myFunctionHandler () {
+    userName = prompt("Enter your name");
 }
 
-button.addEventListener ('click', myFunction);
+button.addEventListener ('click', enterName);
 
 var startButton = document.getElemntById('startGame');
-
-var startGameHanler = function startMygame (){
-    prompt("Enter your name");
+var startGameHandler = function startMygame (){
+    if(usrName == undefined) {
+        alert("Please enter a valid name.");
+    } else {
+    alert("Ready to play.");
+    }
 }
 
 startButton.addEventListener('click', startGameHandler);
+
+var gameButton = function GameButtonConstruct () {
+    var button;
+    var fire = function fireWeaponHandler () {
+        alert("Fire");
+    }
+}
+
+var fireWeaponButton = new gameButton('fireWeapon');
+fireWeaponButton.button = document.getElementId('fireWeapon');
+fireWeaponButton.button.addEventListener('click', fireWeaponButton.fire);
+
+
+
 
